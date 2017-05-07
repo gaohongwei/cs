7e5bfd9a65fdb626f681902e8afc4135ad27d24e
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-05-07 11:45:58 -0700

    Update qsort.rb

--- binary/qsort.rb
+++ binary/qsort.rb
@@ -1,41 +1,50 @@
+# if pivot has the largest one or smallest one
+# it will have o(n*n)
+# have to exclude the pivot to avoid the endless loop
 def qsort(ar,start=0, stop=nil)
   stop ||= ar.length-1
   if  (stop-start)<1
-    return 
+    return
   end
-  if  (stop-start)<2
-    swap(ar,start,stop)
-    return 
-  end
-  pivot=ar[stop]
-  puts ar[start..stop].to_s  
-  puts "pivot=#{pivot}"
-
+  # if  (stop-start)<2
+  #   ar[start]:ar[stop]=ar[stop]:ar[start] if ar[stop]<ar[start]
+  #   return
+  # end
+  pivot = partition(ar,start,stop)
+  qsort(ar,start, pivot) if pivot > (start+1)
+  qsort(ar,pivot+1, stop) if pivot < stop
+end
+def partition(ar,start,stop)
+  pmid = (start+stop)/2
+  pivot=ar[pmid ]
+  puts ar[start..stop].to_s
+  puts "pmid=#{pmid },pivot=#{pivot}"
   ileft=start
-  iright=stop-1
-  while(ileft<iright) 
-    while(ar[ileft]<pivot) 
-      ileft+=1
+  iright=stop
+  while(ileft<iright)
+    # puts "ileft=#{ileft },iright=#{iright}"
+    while ar[ileft] < pivot
+      puts "shift ileft=#{ileft },iright=#{iright}"
+      break if ileft>=iright
+      ileft +=1
     end
-    while(ar[iright]>=pivot) 
+    while ar[iright] >= pivot
+      puts "ileft=#{ileft }, shift iright=#{iright}"
+      break if ileft>=iright
+      iright -=1
+    end
+    if ileft<iright
+      puts ar[start..stop].to_s
+      puts "swap #{ileft }, #{iright}"
+      ar[ileft], ar[iright]= ar[iright], ar[ileft]
+      ileft+=1
       iright-=1
     end
-    puts "swap #{ileft},#{iright}"    
-    swap(ar,ileft,iright)
-    ileft+=1
-    iright-=1
-
-    puts ar[start..stop].to_s
   end
-  swap(ar,ileft,stop)
-  puts ar[start..ileft].to_s
-  puts ar[(ileft+1)..stop].to_s  
-  qsort(ar,start, ileft)
-  qsort(ar,ileft+1, stop)
+  pivot = ileft
+  puts "pivot=#{pivot}"
+  puts ar[start..stop].to_s
+  pivot
 end
 
-def swap(ar,pos1,pos2)
-  tmp=ar[pos1]
-  ar[pos1]=ar[pos2]
-  ar[pos2]=tmp
-end
+ar=(0..9).to_a.shuffle;qsort(ar)
