423f31747d6be0fd82d4650568de883ad371ed99
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-02 15:36:19 -0700

    Update remove_element.rb

--- array/remove_element.rb
+++ array/remove_element.rb
@@ -14,3 +14,17 @@ nums = (1..10).to_a.shuffle
 target = 5
 remove_target(nums,target)
 nums
+
+def remove_target2(nums,target) # remove zero
+  count =0
+  len = nums.length
+  nums.each_with_index do |num,index|
+    if num == target
+      count +=1
+    else
+      nums[index-count] = num
+    end
+  end
+  last = len - count # len - 1 - count +1
+  (last..(len-1)).each {|index| nums[index] = '0'}
+end
