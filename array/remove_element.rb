c4be362083ba66dae32ac14ef5ede6eec7012bb7
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-02 15:41:19 -0700

    Update remove_element.rb

--- array/remove_element.rb
+++ array/remove_element.rb
@@ -10,9 +10,30 @@ def remove_target(nums,target) # remove zero
   (last..(len-1)).each {|index| nums[index] = '*'}
 end
 
+def remove_dup(nums) # for sorted
+  len = nums.length
+  last = 0
+  target = nil
+  nums.each do |num|
+    if num != target
+      target = num
+      nums[last] = num
+      last +=1
+    end
+  end
+  (last..(len-1)).each {|index| nums[index] = '*'}
+end
+
+remove_dup is very simialr to remove_target
+The different is to assign a target
+## test case
 nums = (1..10).to_a.shuffle
 target = 5
 remove_target(nums,target)
+nums
+
+nums =  [1,1,2,2,3,4,4,5,6,6]
+remove_dup(nums)
 nums
 
 def remove_target2(nums,target) # remove zero
