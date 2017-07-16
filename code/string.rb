4150938703e0ed2a6abe325983b6dd5fc50c8211
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-07-16 11:37:15 -0700

    Create string.rb

---
+++ code/string.rb
@@ -0,0 +1,387 @@
+
+1	Code Problem
+1.1	String
+1.1.1	28. Implement strStr()
+Implement strStr().
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+    len_needle = needle.length
+    len_haystack = haystack.length
+    return -1 if len_needle > len_haystack
+    pos = 0
+    (pos..(len_haystack-len_needle))
+end
+
+1.1.2	557. Reverse Words in a String III
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Note: In the string, each word is separated by single space and there will not be any extra space in the string.
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+    
+end
+
+
+1.1.3	551. Student Attendance Record I
+You are given a string representing an attendance record for a student. The record only contains the following three characters:
+
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+
+Example 2:
+Input: "PPALLL"
+Output: False
+
+
+# @param {String} s
+# @return {Boolean}
+def check_record(s)
+    
+end
+
+1.1.4	541. Reverse String II
+Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+Example:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+
+Restrictions:
+The string consists of lower English letters only.
+Length of the given string and k will in the range [1, 10000]
+
+# @param {String} s
+# @param {Integer} k
+# @return {String}
+def reverse_str(s, k)
+    
+end
+
+1.1.5	521. Longest Uncommon Subsequence I
+Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings. The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings.
+
+A subsequence is a sequence that can be derived from one sequence by deleting some characters without changing the order of the remaining elements. Trivially, any string is a subsequence of itself and an empty string is a subsequence of any string.
+
+The input will be two strings, and the output needs to be the length of the longest uncommon subsequence. If the longest uncommon subsequence doesn't exist, return -1.
+
+Example 1:
+Input: "aba", "cdc"
+Output: 3
+Explanation: The longest uncommon subsequence is "aba" (or "cdc"), 
+because "aba" is a subsequence of "aba", 
+but not a subsequence of any other strings in the group of two strings. 
+Note:
+
+Both strings' lengths will not exceed 100.
+Only letters from a ~ z will appear in input strings.
+
+# @param {String} a
+# @param {String} b
+# @return {Integer}
+def find_lu_slength(a, b)
+    
+end
+
+1.1.6	13. Roman to Integer
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
+    
+end
+
+1.1.7	14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+    
+end
+
+1.1.8	520. Detect Capital
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital if it has more than one letter, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+Example 1:
+Input: "USA"
+Output: True
+
+Example 2:
+Input: "FlaG"
+Output: False
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+
+# @param {String} word
+# @return {Boolean}
+def detect_capital_use(word)
+    
+end
+
+1.1.9	20. Valid Parentheses
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "()" and "([])" are not.
+# @param {String} s
+# @return {Boolean}
+def is_valid(s)
+    ar=[]
+    len=s.length
+    index = 0
+    while(index<len)
+        ch= s[index]
+        case ch
+            when '(','[','{'
+                ar.push(ch)
+            when ')'
+               return false if ar.pop != '('
+            when ']'
+               return false if ar.pop != '['            
+            when '}'
+               return false if ar.pop != '{'  
+        end
+        index +=1
+    end
+    ar.empty?
+end
+
+
+1.1.10	459. Repeated Substring Pattern
+Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+
+Example 1:
+Input: "abab"
+
+Output: True
+
+Explanation: It's the substring "ab" twice.
+Example 2:
+Input: "aba"
+
+Output: False
+Example 3:
+Input: "abcabcabcabc"
+
+Output: True
+
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+
+# @param {String} s
+# @return {Boolean}
+def repeated_substring_pattern(s)
+    
+end
+
+1.1.11	606. Construct String from Binary Tree
+You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+
+The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+
+Example 1:
+Input: Binary tree: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /    
+  4     
+
+Output: "1(2(4))(3)"
+
+Explanation: Originallay it needs to be "1(2(4)())(3()())", 
+but you need to omit all the unnecessary empty parenthesis pairs. 
+And it will be "1(2(4))(3)".
+Example 2:
+Input: Binary tree: [1,2,3,null,4]
+       1
+     /   \
+    2     3
+     \  
+      4 
+
+Output: "1(2()(4))(3)"
+
+Explanation: Almost the same as the first example, 
+except we can't omit the first parenthesis pair to break the one-to-one mapping relationship between the input
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} t
+# @return {String}
+def tree2str(t)
+    
+end
+
+1.1.12	434. Number of Segments in a String
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+
+# @param {String} s
+# @return {Integer}
+def count_segments(s)
+    
+end
+
+
+1.1.13	38. Count and Say
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+Example 1:
+
+Input: 1
+Output: "1"
+Example 2:
+
+Input: 4
+Output: "1211"
+
+# @param {Integer} n
+# @return {String}
+def count_and_say(n)
+    
+end
+
+
+1.1.14	383. Ransom Note
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+# @param {String} ransom_note
+# @param {String} magazine
+# @return {Boolean}
+def can_construct(ransom_note, magazine)
+    
+end
+
+1.1.15	345. Reverse Vowels of a String
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+Note:
+The vowels does not include the letter "y".
+
+# @param {String} s
+# @return {String}
+def reverse_vowels(s)
+    
+end
+
+
+1.1.16	344. Reverse String
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+
+
+1.1.17	58. Length of Last Word
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example, 
+Given s = "Hello World",
+return 5.
+
+# @param {String} s
+# @return {Integer}
+def length_of_last_word(s)
+    
+end
+
+
+1.1.18	67. Add Binary
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+
+# @param {String} a
+# @param {String} b
+# @return {String}
+def add_binary(a, b)
+    
+end
+
+1.1.19	125. Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+    
+end
