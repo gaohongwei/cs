d199fd58d9c20e38550525954beb6c0dab7a830d
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-16 00:11:54 -0700

    Create splitter.js

---
+++ string/splitter.js
@@ -0,0 +1,22 @@
+function split_str(str){
+  var splitter = [\s,;]
+  var start=-1, count=0
+  var ch
+  var words = []
+  for(var index=0,len=str.length;index<len;index++){
+    ch = str[index]
+    if( ch.match(/${splitter}/)){
+      if(count>0) words.push(str.substr(start+1,count))
+      count =0
+      start=index
+    } else{
+      count++  
+    }
+  }
+  if(count>0)  words.push(str.substr(start+1,count))
+  return words;
+}
+
+var str = 'abc  , mn kfg'
+console.log(str)
+console.log(split_str(str))
