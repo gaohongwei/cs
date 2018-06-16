d5a2acdcef356fba31854fce036ec0b78a0508ad
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-16 10:29:33 -0700

    Update splitter.js

--- string/splitter.js
+++ string/splitter.js
@@ -1,22 +1,26 @@
-function split_str(str){
-  var splitter = /[\s,;]/
-  var start=-1, count=0
-  var ch
-  var words = []
-  for(var index=0,len=str.length;index<len;index++){
+
+function splitter(str){
+  var splitter = /[\s,;]/;
+  var index, len=str.length
+  var count = 0
+  var ch 
+  var words =[]
+  for(index=0;index<len;index++){
     ch = str[index]
-    if( splitter.test(ch)){
-      if(count>0) words.push(str.substr(start+1,count))
-      count =0
-      start=index
-    } else{
-      count++  
+    if(splitter.test(ch)){
+      if(count > 0)words.push(str.substr(index-count, count))
+      count = 0
+    } else {
+      count++
     }
   }
-  if(count>0)  words.push(str.substr(start+1,count))
-  return words;
+  if(count > 0)words.push(str.substr(index-count, count))
+  return words
 }
 
-var str = 'abc  , mn kfg'
+var str = ' abc a1  , a3 a3 a4  '
+
 console.log(str)
-console.log(split_str(str))
+var words = splitter(str)
+console.log(words)
+// index =5, last =4, count = 2, ,3,4
