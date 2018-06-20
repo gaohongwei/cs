e99b00e245f23976dd538ce8fc323cbc7501d8c0
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-20 15:28:54 -0700

    Create reverse_vowels.rb

---
+++ string/reverse_vowels.rb
@@ -0,0 +1,17 @@
+1.1.15  345. Reverse Vowels of a String
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+Note:
+The vowels does not include the letter "y".
+
+# @param {String} s
+# @return {String}
+def reverse_vowels(s)
+    
+end
