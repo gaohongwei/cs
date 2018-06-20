3dbc144a6db57e725c13e30e9c750583f203c421
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-20 15:27:09 -0700

    Create add_binary.rb

---
+++ string/add_binary.rb
@@ -0,0 +1,15 @@
+
+1.1.18  67. Add Binary
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+
+# @param {String} a
+# @param {String} b
+# @return {String}
+def add_binary(a, b)
+    
+end
