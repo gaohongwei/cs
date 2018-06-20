209b0a843f0454f4a5084880ae07af92c4c049c3
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-20 16:11:38 -0700

    Create longest_common_prefix.rb

---
+++ string/longest_common_prefix.rb
@@ -0,0 +1,9 @@
+1.1.7   14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+    
+end
