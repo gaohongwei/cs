82644e2d1c37fda58e114be308f3c339ff6945be
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-20 15:17:52 -0700

    Update code.rb

--- string/code.rb
+++ string/code.rb
@@ -366,20 +366,3 @@ def add_binary(a, b)
     
 end
 
-1.1.19	125. Valid Palindrome
-Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
-
-For example,
-"A man, a plan, a canal: Panama" is a palindrome.
-"race a car" is not a palindrome.
-
-Note:
-Have you consider that the string might be empty? This is a good question to ask during an interview.
-
-For the purpose of this problem, we define empty string as valid palindrome.
-
-# @param {String} s
-# @return {Boolean}
-def is_palindrome(s)
-    
-end
