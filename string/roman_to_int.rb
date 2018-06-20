a8cd04f3a9c210fe54fece308be4329b3778ffaf
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-20 16:12:04 -0700

    Create roman_to_int.rb

---
+++ string/roman_to_int.rb
@@ -0,0 +1,10 @@
+1.1.6   13. Roman to Integer
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
+    
+end
