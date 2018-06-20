7c5af22c0ab4c58057836cc729ee96525c522a56
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-20 15:42:00 -0700

    Create ransom_note.rb

---
+++ string/ransom_note.rb
@@ -0,0 +1,18 @@
+1.1.14  383. Ransom Note
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+# @param {String} ransom_note
+# @param {String} magazine
+# @return {Boolean}
+def can_construct(ransom_note, magazine)
+    
+end
