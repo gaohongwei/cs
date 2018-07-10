f9aea157a658d7e98954184bb00effacbe8d6d97
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-07-09 19:46:54 -0700

    Update flat_array.rb

--- array/flat_array.rb
+++ array/flat_array.rb
@@ -6,3 +6,22 @@ def flatten_ar(items)
   end
   out
 end
+
+# use loop
+def flatten_ar(items)
+  out = []
+  to_do = items.reverse
+  puts 'array:',items.to_s
+  while(true)
+    break if to_do.length < 1
+    item = to_do.pop
+    if item.is_a?(Array)    
+      to_do += item.reverse
+      puts 'current item:',item.to_s
+      puts 'todo:',to_do.to_s        
+    else
+      out << item
+    end
+  end
+  out
+end
