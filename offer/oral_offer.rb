181ff794476bfd79119c5ccd82f76572d4811596
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-04-18 17:13:41 -0700

    Create oral_offer.rb

---
+++ offer/oral_offer.rb
@@ -0,0 +1,18 @@
+### Response to oral offer
+### Never negotiate a verbal offer.
+  Wow! Thank you for the offer, and I look forward to going over the details in the written offer 
+  before I can give my formal acceptance. 
+  When do you expect a response?
+
+### if HR still asks for oral acceptance, say this
+  As of this time, I see no reason not to accept this position—
+  I  just feel more comfortable having a written offer in hand
+  
+### If HR ask your feedback about the oral offer 
+  1. Try above first
+  2. If HR insist on requiring a feedback
+    If you want to negotiate, start out with a friendly comment to keep things non-adversarial. Say
+    
+    I really appreciate this job offer and I’ve got to tell you I’d love to work here.
+
+    Then make a counter offer.
