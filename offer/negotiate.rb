1b6d64f9255f0bd76797fe567bbd8facfede6155
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-04-18 17:11:16 -0700

    Create negotiate.rb

---
+++ offer/negotiate.rb
@@ -0,0 +1,67 @@
+### Response to oral offer
+### Never negotiate a verbal offer.
+Wow! Thank you for the offer, and I look forward to going over the details in the written offer before I can give my formal acceptance. When do you expect a response?
+
+### if HR still asks for oral acceptance, say this
+  As of this time, I see no reason not to accept this position—I  just feel more comfortable having a written offer in hand
+
+### If HR ask your feedback about the oral offer 
+  1. Try above first
+  2. If they insist on requiring a feedback
+    If you want to negotiate, start out with a friendly comment to keep things non-adversarial. Say
+    I really appreciate this job offer and I’ve got to tell you I’d love to work here.
+
+    Then make a counter offer.
+
+###  Response to the offer
+  Thank you so much again offering me the position. 
+  I’m very excited about the role and looking forward to being part of the team.
+
+  Can I get back to you in few days?”
+
+
+### Negotiate
+### Telling the Employer What You Will Accept/Need
+### Negotiate multiple concerns together
+### Mention requestment one time, dont go back and forth
+
+  Candidate: I am aware that you have budgeted $150,000 for the position. However, I believe that my level of experience warrants a higher rate, should I accept the job.
+
+  HR: How much do you have in mind?
+
+  Candidate: Can you make it $165,000?
+
+  HR: Due to budget constraints and limitations, we can only go as high as $160,000.
+
+  Candidate: I see. Then, may I give you my answer on Friday?
+###
+
+
+### Nogotiate
+  1) Dont leak your current compensation
+
+    I dont feel comfortable disclosing my current compensation. 
+    When I look for jobs, the most important things to me includes: 
+      company, technology,the team
+    Comp is an important part of my consideration, of course, but I’m sure 
+    the company can put together a package that’s 
+      competitive in the market and 
+      comparable within the company
+
+
+  3) Making the counter offer:
+
+  "Based on my experience and credentials I think that an annual base salary of $x is an appropriate market rate because [CITE GLASSDOOR, OTHER OFFERS, OTHER MARKET RESEARCH SUCH AS SALARIES FOR SIMILARLY SITUATED EMPLOYEES, ETC.]. Also, given my unique experience [LIST YOUR QUALIFICATIONS], I feel that it would make sense for [COMPANY] to start me at a higher rate of at least $X base."
+
+  4) Their first response might be, sorry, we cant do anything for you. I would advise holding firm at least once:
+
+  "I totally understand where you’re coming from. I really think that this is a great opportunity for me and I think that this is a great team, but at the same time I think that my qualifications are a great fit and are worth a base salary of $x."
+
+
+  5) They propose something you could be happy with:
+
+  "That’s a great offer. I am really excited about this opportunity but I always like to take my time with important decisions."
+
+  6) End with:
+
+  "When do you need me to get back to you on this?" / "I will definitely get back to you [by Friday / soon]. Thanks again."
